f4a88cc8136c84771d91a5dd6763e9f8734203df
martinfelbaba <martin_felbaba@web.de>
2018-03-05 23:52:18 +0100

    initial commit

---
+++ src/main/java/com/mf/gs/gsjpa/GsJpaApplication.java
@@ -0,0 +1,12 @@
+package com.mf.gs.gsjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsJpaApplication.class, args);
+	}
+}
